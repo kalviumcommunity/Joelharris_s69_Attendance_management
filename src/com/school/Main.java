3e33d604015664f4db4fc8f70316bc945eb3ef8e
Joel Harris <joel.harris@kalvium.community>
2025-08-19 14:00:09 +0530

    Part_01

--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Attendance Management System");
+        System.out.println("Welcome to Attendance Management");
     }
 }
